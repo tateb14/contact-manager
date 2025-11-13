5480723af271b652b4b27d5653198a911ef375c6
tate.b <121896551+tateb14@users.noreply.github.com>
2025-11-13 13:04:20 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def menu():
+    pass
+
+def add_contact():
+    pass
+
+def search_contact():
+    pass
+
+def edit_contact():
+    pass
+
+def delete_contact():
+    pass
+
+def display_contacts():
+    pass
+
