f48640539503197883b7f09255e21a5ab745a8c8
tate.b <121896551+tateb14@users.noreply.github.com>
2025-11-18 12:46:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,22 @@ def display_contacts():
     pass
 
 def main():
-    pass
+    # main accepts no args
+    # takes choice from menu() and finds the proper function to call
+    # calls the proper function.
+    
+    # call menu and get choice
+    choice = menu()
+    
+    if choice == 1:
+        add_contact()
+    elif choice == 2:
+        search_contact()
+    elif choice == 3:
+        edit_contact()
+    elif choice == 4:
+        delete_contact()
+    elif choice == 5:
+        display_contacts()
+    else:
+        print("Thanks for using the contact manager!")
