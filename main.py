51d8a90ed951a9683d8e81ec97df2c25fae73448
AlexW2010 <askforarose11@gmail.com>
2025-11-18 13:04:09 -0600

    finish add_contacts

--- main.py
+++ main.py
@@ -3,8 +3,42 @@ import os
 def menu():
     pass
 
-def add_contact():
-    pass
+def add_contact():#add_contact accepts no arguements
+    #add contact adds a contact to the file
+    
+    #make file contacts.txt
+    infile = open("contacts.txt", 'a')
+    
+    
+    try: #validate input for num_contacts
+        num_contacts = int(input("How many contacts would you like to add? "))
+        
+    except ValueError: 
+        print("ERROR: Non-numeric date found, calculations haulted")
+    
+    #loop for all contacts
+    for num in range(1, num_contacts +1):
+        
+        #get input of name, street adress, phone number, and email adress
+        print(f"Information for contact number {num}.")
+        name = input("Please input your name: ")
+        street_address = input("Please input your street address: ")
+        phone_number = input("Please input your phone number: ")
+        email_address = input("Please input your email address: ")
+        
+        #store the file
+        
+        infile.write(name + '\n')
+        infile.write(street_address + '\n')
+        infile.write(phone_number + '\n')
+        infile.write(email_address + '\n')
+        
+     #close the file
+    infile.close()
+    print("All input has been saved in contacts.txt")
+        
+    
+    
 
 def search_contact():
     pass
@@ -15,8 +49,11 @@ def edit_contact():
 def delete_contact():
     pass
 
-def display_contacts():
-    pass
+def display_contacts(): #display contact accepts no arguements
+    #display contacts function that displays all contacts
+    #and their information in a readable format
+    
+    contact_file = open('contacts.txt', 'r')
 
 def main():
     pass
